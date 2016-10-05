a3221ecdf72877546214cfc2da9f768c42fd93cb
Matthias Brettschneider <frosch03@gmail.com>
2016-10-05 23:00:44 +0200

    updated bitlbee version, package dependecies and bitlbee url

--- build.sh
+++ build.sh
@@ -2,13 +2,13 @@
 set -e
 cd `dirname $0`
 
-VERSION=3.2.1
+VERSION=3.4.2
 
 # Dependencies
 apt-get update -qy
-DEBIAN_FRONTEND=noninteractive apt-get install -qy curl build-essential libglib2.0-dev libgnutls-dev libotr2-dev
+DEBIAN_FRONTEND=noninteractive apt-get install -qy curl build-essential libglib2.0-dev libgnutls28-dev libotr5-dev
 
-curl http://get.bitlbee.org/src/bitlbee-${VERSION}.tar.gz | tar xzvf - -C /usr/src
+curl https://get.bitlbee.org/src/bitlbee-${VERSION}.tar.gz | tar xzvf - -C /usr/src
 
 # Build & install
 cd /usr/src/bitlbee-${VERSION}
